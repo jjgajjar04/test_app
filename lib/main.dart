8b7a2c8b00cb5a259fcc30aa2e6fc10f3e37191c
ekmbuma <ambuj.a.kumar@ericsson.com>
2020-09-16 21:30:41 +0530

    initial commit

---
+++ lib/main.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('Test App'),
+          actions: [Icon(Icons.more_vert)],
+        ),
+      ),
+    );
+  }
+}
