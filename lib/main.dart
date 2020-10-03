0ef60b56bba57b1f5925f55d02f7cd9d6319bd2b
AmbujaAK <itsambuja@gmail.com>
2020-10-04 04:17:33 +0530

    added device preview

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:test_app/my_app.dart';
+import 'package:device_preview/device_preview.dart';
 
-void main() => runApp(MyApp());
+void main() => runApp(
+      DevicePreview(
+        enabled: false,
+        builder: (context) => MyApp(),
+      ),
+    );
