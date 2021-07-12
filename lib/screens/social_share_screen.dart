34b994a1573df79d2c2105346f873de939172883
AmbujaAK <itsambuja@gmail.com>
2021-07-13 00:12:00 +0530

    added web support share : inprogress

--- lib/screens/social_share_screen.dart
+++ lib/screens/social_share_screen.dart
@@ -1,9 +1,13 @@
+import 'dart:js_util';
+import 'dart:html' as html;
+
 import 'package:flutter/material.dart';
+import 'package:share_plus_web/share_plus_web.dart';
 import 'package:test_app/utils/constants.dart';
 // import 'package:image_picker/image_picker.dart';
 import 'package:share_plus/share_plus.dart';
 import 'package:flutter/foundation.dart' show kIsWeb;
-
+import 'package:url_launcher/url_launcher.dart';
 // import 'image_previews.dart';
 
 class SocialShareScreen extends StatefulWidget {
@@ -18,6 +22,7 @@ class _SocialShareScreenState extends State<SocialShareScreen> {
 
   String subject = '';
 
+  var _navigator = html.window.navigator;
   List<String> imagePaths = [];
 
   @override
@@ -81,9 +86,12 @@ class _SocialShareScreenState extends State<SocialShareScreen> {
                   Builder(
                     builder: (BuildContext context) {
                       return ElevatedButton(
-                        onPressed: text.isEmpty && imagePaths.isEmpty
-                            ? null
-                            : () => _onShare(context),
+                        onPressed: () {
+                          _onShareWeb(context);
+                        },
+                        // onPressed: text.isEmpty && imagePaths.isEmpty
+                        //     ? null
+                        //     : () => _onShare(context),
                         child: const Text('Share'),
                       );
                     },
@@ -101,14 +109,54 @@ class _SocialShareScreenState extends State<SocialShareScreen> {
     });
   }
 
+  Future<void> _onShareWeb(BuildContext context) async {
+    try {
+      await _navigator.share({'title': subject, 'text': text});
+    } on NoSuchMethodError catch (_) {
+      print('Unable to share on web : CATCH');
+
+      //Navigator is not available or the webPage is not served on https
+      final queryParameters = {
+        if (subject != null) 'subject': subject,
+        'body': text,
+      };
+
+      // see https://github.com/dart-lang/sdk/issues/43838#issuecomment-823551891
+      final uri = Uri(
+        scheme: 'mailto',
+        query: queryParameters.entries
+            .map((e) =>
+                '${Uri.encodeComponent(e.key)}=${Uri.encodeComponent(e.value)}')
+            .join('&'),
+      );
+
+      if (await canLaunch(uri.toString())) {
+        print('Unable to share on web : IF');
+        print(uri.toString());
+        await launch(uri.toString());
+      } else {
+        print('Unable to share on web');
+        throw Exception('Unable to share on web');
+      }
+    }
+  }
+
   void _onShare(BuildContext context) async {
     final box = context.findRenderObject() as RenderBox;
 
     if (kIsWeb) {
-      await Share.share(
-        text,
-        subject: subject,
-      );
+      print('web');
+      await _navigator.share({'title': subject, 'text': text});
+
+      // await SharePlusPlugin().share(
+      //   text,
+      //   subject: subject,
+      //   sharePositionOrigin: box.localToGlobal(Offset.zero) & box.size,
+      // );
+      // await Share.share(
+      //   text,
+      //   subject: subject,
+      // );
     } else {
       await Share.share(
         text,
