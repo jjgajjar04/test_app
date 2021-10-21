851d6fd041bdeecd17d7779512f1afde5d19c90c
nikunjdgnr2 <55309894+nikunjdgnr2@users.noreply.github.com>
2021-10-21 14:31:05 +0530

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,17 +1,15 @@
 import 'package:flutter/material.dart';
 import 'package:test_app/my_app.dart';
-import 'dart:io' show Platform;
 import 'package:flutter/foundation.dart';
 import 'package:device_preview/device_preview.dart';
 import 'package:device_preview/plugins.dart';
 import 'package:flutter/cupertino.dart';
-import 'dart:io';
-
 import 'package:test_app/service_locator.dart' as di;
+import 'dart:io' show Platform;
+import 'dart:io';
 
 void main() {
   di.init();
-  // Desktop platforms aren't a valid platform.
   if (!kIsWeb) _setTargetPlatformForDesktop();
 
   bool preview = false;
@@ -40,17 +38,14 @@ void main() {
     );
 }
 
-/// If the current platform is desktop, override the default platform to
-/// a supported platform (iOS for macOS, Android for Linux and Windows).
-/// Otherwise, do nothing.
 void _setTargetPlatformForDesktop() {
   TargetPlatform? targetPlatform;
-  if (Platform.isMacOS) {
+  if (Platform.isMacOS) 
     targetPlatform = TargetPlatform.iOS;
-  } else if (Platform.isLinux || Platform.isWindows) {
+   else if (Platform.isLinux || Platform.isWindows) 
     targetPlatform = TargetPlatform.android;
-  }
-  if (targetPlatform != null) {
+  
+  if (targetPlatform != null) 
     debugDefaultTargetPlatformOverride = targetPlatform;
-  }
+  
 }
